afe72952ef6d78fb650554dc45aee5e6c347c7e6
minhaskhan <60728143+minhaskhan@users.noreply.github.com>
2020-02-29 12:06:23 -0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Member(models.Model):
+        fname = models.CharField(max_length=50)
+        lname =  models.CharField(max_length=100)
+        email = models.EmailField(max_length=200)
+        passwd = models.CharField(max_length=50)
+        age = models.IntegerField()
+
+        def __str__(self):
+                return self.fname + ' ' + self.lname
---
+++ views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from . models import  Member
+from . forms import MemberForm
+from django.contrib import messages
+
+def home(request):
+    all_members = Member.objects.all
+    return render(request, 'home.html', {'all':all_members})
+
+def join(request):
+    if request.method == "POST":
+        form = MemberForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            age = request.POST['age']
+            email = request.POST['email']
+            passwd = request.POST['passwd']
+
+            messages.success(request, ('There was an error in your form ...Please try again ...'))
+           # return redirect('join')
+            return render(request, 'join.html', {
+                'fname': fname,
+                'lname': lname,
+                'age': age,
+                'email': email,
+                'passwed': passwd,
+            })
+
+        messages.success(request, ('Your form Has Been Successfully Sumitted!'))
+        return redirect('home')
+    else:
+        return render(request, 'join.html', {})
